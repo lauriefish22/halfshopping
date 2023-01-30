91b56e8bd8e0be778030bc2757d6eb77b565c588
Laurie Fish <iamlauriefish@gmail.com>
2023-01-30 15:06:28 -0800

    createTodo function

--- fetch-utils.js
+++ fetch-utils.js
@@ -4,30 +4,31 @@ const SUPABASE_KEY =
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-export async function createTodo(todo) {
-    // create a single incomplete todo with the correct 'todo' property for this user in supabase
-
-    // once you have a response from supabase, comment this back in:
-    // return checkError(response);
-}
+export async function createTodo() {
+    const { data, error } = await client
+    .from('todos')
+    .insert {
+        todo: todo
+    }
+    return data;
+    
+};
+    
 
 export async function deleteAllTodos() {
     // delete all todos for this user in supabase
-
     // once you have a response from supabase, comment this back in:
     // return checkError(response);
 }
 
 export async function getTodos() {
-    // get all todos for this user from supabase
+    const { data } = await client.from('todo').select('*');
 
-    // once you have a response from supabase, comment this back in:
-    // return checkError(response);
+    return data;
 }
 
 export async function completeTodo(id) {
     // find the and update (set complete to true), the todo that matches the correct id
-
     // once you have a response from supabase, comment this back in:
     // return checkError(response);
 }
